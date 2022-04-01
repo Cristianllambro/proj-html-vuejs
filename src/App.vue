14ffd41d8b69ea82a9cf914832d6c40b1ec8849c
cristianllambro <llambro.cristian1996@gmail.comm>
2022-04-01 15:42:51 +0200

    start work, add img

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderContent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderContent from './components/HeaderContent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderContent
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav">
+      <h1>ciao</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderContent',
+}
+</script>
+
+<style>
+
+</style>
